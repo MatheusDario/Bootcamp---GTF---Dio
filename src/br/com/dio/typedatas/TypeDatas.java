fbfc9b03e67d9cb5bef53cd734595dcb1ff960c6
MatheusDario <dario.matheus97@gmail.com>
2022-03-04 14:57:10 -0300

    about variables

---
+++ src/br/com/dio/typedatas/TypeDatas.java
@@ -0,0 +1,18 @@
+package br.com.dio.typedatas;
+
+public class TypeDatas {
+    public static void main(String[] args) {
+        byte b1 = 10;
+        short s1 = 30000;
+        int i1 = 1000000000;
+        long l1 = 1000000000000000000l;
+        float f1 = 4.5f;
+        double d1 = 0.5;
+        char c1 = 'M';
+        String st1 = "Chegando da festa";
+        boolean bo1 = true;
+
+        short i = 3;
+        System.out.println(10-5*2+ --i);
+    }
+}
---
+++ src/br/com/dio/variaveis/AboutVariable.java
@@ -0,0 +1,11 @@
+package br.com.dio.variaveis;
+
+public class AboutVariable {
+    public static void main(String[] args) {
+        int i;
+        i = 5;
+        final int j = 10;
+        int quantidadeProdutos = 50;
+        System.out.println(quantidadeProdutos);
+    }
+}
