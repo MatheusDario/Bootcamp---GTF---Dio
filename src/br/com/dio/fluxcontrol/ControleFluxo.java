84f6fdf023234a5b35a0863b63ca41a1b08d4f08
MatheusDario <dario.matheus97@gmail.com>
2022-03-12 09:37:20 -0300

    about methods

---
+++ src/br/com/dio/fluxcontrol/ControleFluxo.java
@@ -0,0 +1,38 @@
+package br.com.dio.fluxcontrol;
+
+public class ControleFluxo {
+    public static void main(String[] args) {
+        mesesAno();
+    }
+    public static void mesesAno() {
+        int mes = 13;
+        if(mes == 1) {
+            System.out.println("Janeiro");
+        } else if (mes == 2) {
+            System.out.println("Fevereiro");
+        } else if (mes == 3) {
+            System.out.println("Março");
+        } else if (mes == 4) {
+            System.out.println("Arbil");
+        } else if (mes == 5) {
+            System.out.println("Maio");
+        } else if (mes == 6) {
+            System.out.println("Junho");
+        } else if (mes == 7) {
+            System.out.println("Julho");
+        } else if (mes == 8) {
+            System.out.println("Agosto");
+        } else if (mes == 9) {
+            System.out.println("Setembro");
+        } else if (mes == 10) {
+            System.out.println("Outubro");
+        } else if (mes == 11) {
+            System.out.println("Novembro");
+        } else if (mes == 12) {
+            System.out.println("Dezembro");
+        } else {
+            System.out.println("Dia do mes invalido");
+        }
+    }
+
+}
