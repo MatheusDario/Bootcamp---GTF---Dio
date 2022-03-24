aa4350b01bee8f5da88d7f85a7888233adaf9228
MatheusDario <dario.matheus97@gmail.com>
2022-03-24 18:11:37 -0300

    ex3

--- src/br/com/dio/loops/Ex3_MaiorMedia.java
+++ src/br/com/dio/loops/Ex3_MaiorMedia.java
@@ -1,4 +1,24 @@
 package br.com.dio.loops;
 
+import java.util.Scanner;
+
 public class Ex3_MaiorMedia {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int count = 0, numero = 0, maior =0, soma = 0, media = 0;
+
+        do {
+            System.out.println("Numero: ");
+            numero = sc.nextInt();
+            soma = soma + numero;
+            if(numero > maior) maior = numero;
+            count ++;
+            media = soma / count;
+        } while (count < 5);
+
+        System.out.println("Maior: " + maior);
+        System.out.println("Media: " + media);
+
+    }
 }
