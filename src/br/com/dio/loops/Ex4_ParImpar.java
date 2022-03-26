11aa479a8d06977a09bf3bec8870e07f2e5bcd79
MatheusDario <dario.matheus97@gmail.com>
2022-03-25 21:32:13 -0300

    ex4

--- src/br/com/dio/loops/Ex4_ParImpar.java
+++ src/br/com/dio/loops/Ex4_ParImpar.java
@@ -1,4 +1,23 @@
 package br.com.dio.loops;
-
+import java.util.Scanner;
 public class Ex4_ParImpar {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int qntNumeros, numeros;
+        int qntPares = 0, qntImpares = 0;
+
+        System.out.println("Quantas entradas: ");
+        qntNumeros = sc.nextInt();
+
+        for(int i=0; i<qntNumeros; i++) {
+            System.out.println("Insira os Numeros: ");
+            numeros = sc.nextInt();
+
+            if(numeros % 2 == 0) qntPares++;
+            else qntImpares++;
+        }
+
+        System.out.println("Tem " + qntPares + " numeros pares");
+        System.out.println("Tem " + qntImpares + " numeros Impares");
+    }
 }
