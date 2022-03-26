8b1af5030873b363067f414678ba84e21cc64adb
MatheusDario <dario.matheus97@gmail.com>
2022-03-25 21:39:13 -0300

    ex5

--- src/br/com/dio/loops/Ex5_Tabuada.java
+++ src/br/com/dio/loops/Ex5_Tabuada.java
@@ -1,4 +1,17 @@
 package br.com.dio.loops;
 
+import java.util.Scanner;
+
 public class Ex5_Tabuada {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Tabuada: ");
+        int tabuada = sc.nextInt();
+        System.out.println("Tabuada de " + tabuada);
+
+        for(int i=1; i <=10; i++) {
+            System.out.println(tabuada + " X " + i + " = " + (tabuada*i));
+        }
+    }
 }
